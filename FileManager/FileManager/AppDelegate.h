c90af76151b1846f5a5c6fa62b4631a035300726
Saurabh Sharma <saurabhsharma@live.com>
2013-12-30 15:10:40 +0530

    import

---
+++ FileManager/FileManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileManager
+//
+//  Created by Saurabh Sharma on 30/12/13.
+//  Copyright (c) 2013 Saurabh Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FileManager/FileManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FileManager
+//
+//  Created by Saurabh Sharma on 30/12/13.
+//  Copyright (c) 2013 Saurabh Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
